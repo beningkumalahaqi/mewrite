db02bb91b3d56d6f609825efd0e96ce8abef649b
beningkumalahaqi <bkhaqi@gmail.com>
2026-09-01 15:24:37 +0700

    feat: add MeWriteLogo to navbar

--- me-write-app/components/public/nav.tsx
+++ me-write-app/components/public/nav.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function PublicNav() {
   return (
     <nav className="nav">
-      <Link href="/" className="nav__brand">MeWrite</Link>
+      <Link href="/" className="nav__brand">
+        <Image src="/MeWriteLogo.png" alt="MeWrite" width={28} height={28} priority />
+        MeWrite
+      </Link>
       <div className="nav__links">
         <Link href="/" className="active">Writings</Link>
         <Link href="/desk">Desk</Link>
